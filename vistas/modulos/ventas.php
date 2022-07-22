ab3076e653384cd5c089cd6b3d8ca3b857e7addd
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-07-22 09:33:47 -0600

    Creando el objeto para eliminar venta

--- vistas/modulos/ventas.php
+++ vistas/modulos/ventas.php
@@ -105,7 +105,7 @@
 
                       <button class="btn btn-warning btnEditarVenta" idVenta="'.$value["id"].'"><i class="fa fa-pencil"></i></button>
 
-                      <button class="btn btn-danger btnEliminarVenta"><i class="fa fa-times"></i></button>
+                      <button class="btn btn-danger btnEliminarVenta" idVenta="'.$value["id"].'"><i class="fa fa-times"></i></button>
 
                     </div>  
 
@@ -120,6 +120,13 @@
         </tbody>
 
        </table>
+
+       <?php
+
+        $eliminarVenta = new ControladorVentas();
+        $eliminarVenta -> ctrEliminarVenta();
+
+      ?>
 
       </div>
 
