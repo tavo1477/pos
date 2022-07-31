68c2e2000879c47e8a7afd1fa674502e14f6dd58
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-07-31 16:13:47 -0600

    Ordenando las ventas

--- vistas/modulos/editar-venta.php
+++ vistas/modulos/editar-venta.php
@@ -145,8 +145,9 @@
 
                       $item = "id";
                       $valor = $value["id"];
+                      $orden = "id";
 
-                      $respuesta = ControladorProductos::ctrMostrarProductos($item, $valor);
+                      $respuesta = ControladorProductos::ctrMostrarProductos($item, $valor, $orden);
 
                       $stockAntiguo = $respuesta["stock"] + $value["cantidad"];
                       
