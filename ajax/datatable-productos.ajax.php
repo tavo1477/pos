35a466e4ceafadf96b25888fdab5043b073bd3fc
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-07-31 16:12:18 -0600

    Ordenando las ventas

--- ajax/datatable-productos.ajax.php
+++ ajax/datatable-productos.ajax.php
@@ -17,8 +17,9 @@ class TablaProductos{
 
 		$item = null;
 		$valor = null;
+		$orden = "id";
 
-		$productos = ControladorProductos::ctrMostrarProductos($item, $valor);	
+		$productos = ControladorProductos::ctrMostrarProductos($item, $valor, $orden);	
 
 		$datosJson = '{
 		  "data": [';
