03cf2962714146a57e0062ab96e0b7724a7e334b
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-08-01 14:46:27 -0600

    Creamos el modelo para sumar el total de las ventas

--- modelos/ventasModelo.php
+++ modelos/ventasModelo.php
@@ -181,4 +181,22 @@ class ModeloVentas{
 
 	}
 
+	/*=============================================
+	SUMA TOTAL VENTAS
+	=============================================*/	
+
+	static public function mdlSumaTotalVentas($tabla){
+
+		$stmt = Conexion::conectar()->prepare("SELECT SUM(neto) as total FROM $tabla");
+
+		$stmt -> execute();
+
+		return $stmt -> fetch();
+
+		$stmt -> close();
+
+		$stmt = null;
+
+	}
+
 }
