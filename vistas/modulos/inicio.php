d176375f729aa0d022b48c6040aeb2ad157e7286
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-08-01 14:45:10 -0600

    Vinculamos el controlador de ventas para las cajas-superiores

--- vistas/modulos/inicio.php
+++ vistas/modulos/inicio.php
@@ -1,6 +1,4 @@
-<div class="content-wrapper">
-
-    <!-- Content Header (Page header) -->
+<div class="content-wrapper"> 
 
     <section class="content-header">
 
@@ -20,33 +18,18 @@
 
     </section>
 
-    <!-- Main content -->
     <section class="content">
 
-      <!-- Default box -->
-      <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
+      <div class="row">
+        
+        <?php 
 
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                    title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button>
-          </div>
-        </div>
-        <div class="box-body">
-          Start creating your amazing application!
-        </div>
-        <!-- /.box-body -->
-        <div class="box-footer">
-          Footer
-        </div>
-        <!-- /.box-footer-->
-      </div>
-      <!-- /.box -->
+          include "inicio/caja-superiores.php";
+
+         ?>
+
+      </div>           
 
     </section>
-    <!-- /.content -->
+ 
 </div>
