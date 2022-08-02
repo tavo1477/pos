edd983b68dfa8931563e334c2785cbd4a455bdcc
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-08-02 12:29:06 -0600

    Creando roles de perfil al menú

--- vistas/modulos/menu.php
+++ vistas/modulos/menu.php
@@ -1,127 +1,147 @@
 <aside class="main-sidebar">
-	
-	<section class="sidebar">
-		
-		<ul class="sidebar-menu">
-		
-			<li class="active">
-				
-				<a href="inicio">
-					
-					<i class="fa fa-home"></i>
 
+	 <section class="sidebar">
+
+		<ul class="sidebar-menu">
+
+		<?php
+
+		if($_SESSION["perfil"] == "Administrador"){
+
+			echo '<li class="active">
+
+				<a href="inicio">
+
+					<i class="fa fa-home"></i>
 					<span>Inicio</span>
 
-				</a>           
+				</a>
 
 			</li>
 
 			<li>
-				
+
 				<a href="usuarios">
-					
+
 					<i class="fa fa-user"></i>
+					<span>Usuarios</span>
 
-					<span>Gestor usuarios</span>
+				</a>
 
-				</a>           
+			</li>';
 
-			</li>
+		}
 
-			<li>
-				
+		if($_SESSION["perfil"] == "Administrador" || $_SESSION["perfil"] == "Especial"){
+
+			echo '<li>
+
 				<a href="categorias">
-					
+
 					<i class="fa fa-th"></i>
+					<span>Categorías</span>
 
-					<span>Gestor categorías</span>
-
-				</a>           
+				</a>
 
 			</li>
 
 			<li>
-				
+
 				<a href="productos">
-					
+
 					<i class="fa fa-product-hunt"></i>
+					<span>Productos</span>
 
-					<span>Gestor productos</span>
+				</a>
 
-				</a>           
+			</li>';
 
-			</li>
+		}
 
-			<li>
-				
+		if($_SESSION["perfil"] == "Administrador" || $_SESSION["perfil"] == "Vendedor"){
+
+			echo '<li>
+
 				<a href="clientes">
-					
-					<i class="fa fa-user"></i>
 
-					<span>Gestor clientes</span>
+					<i class="fa fa-users"></i>
+					<span>Clientes</span>
 
-				</a>           
+				</a>
 
-			</li>
+			</li>';
 
-			<li class="treeview">
-				
-				<a href="">
-					
+		}
+
+		if($_SESSION["perfil"] == "Administrador" || $_SESSION["perfil"] == "Vendedor"){
+
+			echo '<li class="treeview">
+
+				<a href="#">
+
 					<i class="fa fa-list-ul"></i>
-
-					<span>Gestor ventas</span>
-
+					
+					<span>Ventas</span>
+					
 					<span class="pull-right-container">
-						
+					
 						<i class="fa fa-angle-left pull-right"></i>
 
 					</span>
 
-				</a> 
+				</a>
 
 				<ul class="treeview-menu">
-
-					<li>
-						
-						<a href="ventas">
-
-							<i class="fa fa-circle-o"></i>
-							<span>Administrar ventas</span>						
-
-						</a>
-
-					</li>
-
-					<li>
-						
-						<a href="crear-venta">
-
-							<i class="fa fa-circle-o"></i>
-							<span>Crear venta</span>						
-
-						</a>
-
-					</li>
-
-					<li>
-						
-						<a href="reportes">
-
-							<i class="fa fa-circle-o"></i>
-							<span>Reporte de ventas</span>						
-
-						</a>
-
-					</li>
 					
+					<li>
 
-				</ul>          
+						<a href="ventas">
+							
+							<i class="fa fa-circle-o"></i>
+							<span>Administrar ventas</span>
 
-			</li>
+						</a>
+
+					</li>
+
+					<li>
+
+						<a href="crear-venta">
+							
+							<i class="fa fa-circle-o"></i>
+							<span>Crear venta</span>
+
+						</a>
+
+					</li>';
+
+					if($_SESSION["perfil"] == "Administrador"){
+
+					echo '<li>
+
+						<a href="reportes">
+							
+							<i class="fa fa-circle-o"></i>
+							<span>Reporte de ventas</span>
+
+						</a>
+
+					</li>';
+
+					}
+
+				
+
+				echo '</ul>
+
+			</li>';
+
+		}
+
+		?>
 
 		</ul>
 
-	</section>
+	 </section>
 
 </aside>
